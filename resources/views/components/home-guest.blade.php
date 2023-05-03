345b278e7db8b43db34546f0f6be340d17659b2f
Ardian1408 <ardi3665@gmail.com>
2023-05-03 21:08:33 +0700

    init

--- resources/views/components/home-guest.blade.php
+++ resources/views/components/home-guest.blade.php
@@ -1,6 +1,6 @@
 <div class=" grid grid-cols-9 gap-4 bg-white mt-20 rounded-md p-4 md:w-[42rem] sm:w-[32rem] w-[20rem] shadow-md shadow-black/50">
   <div class="md:col-span-3 sm:col-span-6 col-span-full flex flex-col gap-y-2 items-center justify-start h-full text-center">
-    <img src="" class="w-10 h-10 object-contain object-center" alt="">
+    <img src="images\tiktok-3-256.png" class="w-10 h-10 object-contain object-center" alt="">
     <h3 class="text-amber-400 font-medium">Deskripsi Usaha</h3>
     <p class="text-neutral-400 text-sm font-light">Fitur yang dapat membantu mitra memperkenalkan usahanya yang dapat membantu pendana untuk memberikan modal  </p>
   </div>
